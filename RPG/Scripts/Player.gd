8ebf074b81d4abc6f526094b64e8500a7c15648a
SrAkuVT <srakuvt@gmail.com>
2024-02-29 01:48:08 -0500

    Texture and move
    Añadí Texturas y Movimiento al pj

--- RPG/Scripts/Player.gd
+++ RPG/Scripts/Player.gd
@@ -1,28 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const velocidad = 100.0
+@onready var player = $"."
+@onready var animation = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta):
+	
+	if Input.is_action_pressed("ARRIBA"):
+		animation.play("Arriba")
+		player.position.y -= velocidad * _delta
+	elif Input.is_action_pressed("IZQUIERDA"):
+		animation.play("Izquierda")
+		player.position.x -= velocidad * _delta
+	elif Input.is_action_pressed("DERECHA"):
+		animation.play("Derecha")
+		player.position.x += velocidad * _delta
+	elif Input.is_action_pressed("ABAJO"):
+		animation.play("Abajo")
+		player.position.y += velocidad * _delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		animation.stop()
 	move_and_slide()
